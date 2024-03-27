275c93a263c7c1d54764077cede065f563e5183c
YangGwonMo <tony577@naver.com>
2024-03-27 17:52:00 +0900

    Q1019 백준

--- src/bakjoon/Q1019.java
+++ src/bakjoon/Q1019.java
@@ -5,31 +5,55 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Q1019 {
-    // 주어지는 페이지 숫자
-    private static int totalPage;
-    private static int[] nums;
+    // 숫자가 얼마나 나오는지 담아놓을 배열
+    static int page[] = new int[10];
+    // 10단위씩 뛰면서 계산하기 위해
+    static int count = 1;
     public static void main(String[] args) throws IOException{
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String totalPageStr = reader.readLine(); 
+        int B = Integer.parseInt(reader.readLine());
+        int A = 1;
 
-        totalPage = Integer.parseInt(totalPageStr);
+        while (A <= B) {
+            //4567
+            // 1의 자리를 9로 맞추는 과정 -> 그 과정시 나온 숫자 카운트
+            while(B%10 != 9 && A <= B){
+                cal(B);
+                B--;
+            }
+            // 시작페이지를 10으로
+            while(A%10 != 0 && A <= B) {
+                cal(A);
+                A++;
+            }
+            // 10 ~ 4559
+            // 1의 자리 9로 맞춘 후 10단위씩 확인하면서 올라간다
+            if(A > B) {
+                break;
+            }
+
+            A /= 10;
+            B /= 10;
+            
+            // 1 ~ 455
+            for(int i=0; i<10; i++){
+                page[i] += (B-A+1)*count;
+            }
+
+            // 한번 진행하고 10자리 100자리는 해당 번호 자리수가 해당번호 자리수 만큼 나온다
+            count *= 10;
+        }
+
+        for(int i=0; i <10; i++){
+            System.out.print(page[i]+ " ");
+        }
         
-        nums = new int[10];
-        for(int n=0; n<nums.length; n++){
-            nums[n] = 0;
-        }
-        for(int i=0; i<totalPage; i++){
-            countNum(totalPage);
-        }
-        for(int j=0; j<nums.length; j++){
-            System.out.print(nums[j]);
-        }
     }
-    
-    private static void countNum(int num){
-        while(num>0){
-            nums[num%10]++;
-            num = num/10;
+
+    public static void cal(int cur){
+        while(0< cur){
+            page[cur % 10] += count;
+            cur /= 10;
         }
     }
 }
